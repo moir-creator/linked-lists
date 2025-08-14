7713b5a5016b73d8b931ab3d43d832ce39afeec6
Alexandr Zaporojan <alexandr.zaporojan@gmail.com>
2025-08-14 09:40:53 +0300

    Update doubleLinkedList.cpp
    insert "tail"

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -5,7 +5,10 @@ struct Node {
     Node* next;
 };
 
+//добавить tail и его обработку
+//Output в прямом и обратном порядке
 Node* head = nullptr;
+Node* tail = nullptr;
 
 void addFirst(int data) {
     head = new Node(nullptr, data, head);
@@ -14,6 +17,7 @@ void addFirst(int data) {
 void add(int data) {
     if (head == nullptr) {
         head = new Node(nullptr, data, nullptr);
+        tail = head;
         return;
     }
     else {
@@ -22,27 +26,40 @@ void add(int data) {
             tmp = tmp->next;
         }
         tmp->next = new Node(tmp, data, nullptr);
+        tail = tmp->next;
     }
 }
 
 void addSorted(int data) {
 if (head == nullptr) {
     head = new Node(nullptr, data, nullptr);
+    tail = head;
     return;
 }
 if (data < head->data) {
     head = new Node(nullptr, data, head);
     head->next->prev = head;
+    //tail = head->next;
     return;
     }
 Node* tmp = head->next;
-Node* prev = head;
+Node* p = head;
     while (tmp != nullptr && tmp->data < data) {
-        prev = tmp;
+        p = tmp;
         tmp = tmp->next;
     }
-    prev->next = new Node(prev->prev, data, tmp);
-    prev->next->prev = prev;
+    p->next = new Node(p->prev, data, tmp);
+    p->next->prev = p;
+    if (tmp == nullptr) {
+        tail = p->next;
+    }
+    if (p->next->next != nullptr) {
+        p->next->next->prev = p->next;
+        if (tmp == nullptr) {
+            tail = tmp;
+        }
+    }
+
 }
 
 void bubleSort_() {
@@ -65,10 +82,11 @@ void bubleSort() {
             Node* tmp = head;
             head = head->next;
             tmp->next = head->next;
-            tmp->prev = head;
+            tmp->next->prev = head;
             head->next = tmp;
             head->prev = nullptr;
-            tmp->next->prev = tmp;
+            head->next->prev = head;
+            head->next->next->prev = head->next;
             changed = true;
         }
         Node* tmp = head;
@@ -79,44 +97,91 @@ void bubleSort() {
                 tmp->next->prev = tmp;
                 t->next = tmp->next->next;
                 t->prev = tmp->next;
+                if (t->next != nullptr) {
+                    t->next->prev = t;
+                }
                 tmp->next->next = t;
                 changed = true;
             }
             tmp = tmp->next;
+            tail = tmp->next;
         }
     }
 }
 
-void output() {
-        Node* tmp = head;
-        while (tmp != nullptr) {
-            std::cout << tmp->data << " ";
-            tmp = tmp->next;
+bool remove (int value) {
+    if (head == nullptr) {
+        return false;
+    }
+    if (head->data == value) {
+        Node* tmp = head->next;
+        delete head;
+        head = tmp;
+        head->prev = nullptr;
+        return true;
+    }
+    Node* tmp = head;
+    while (tmp->next != nullptr) {
+        if (tmp->next->data == value) {
+            Node* t = tmp->next;
+            tmp->next = tmp->next->next;
+            tmp->next->prev = tmp;
+            delete t;
+            return true;
         }
+        tmp = tmp->next;
+    }
+    return false;
 }
 
+void removeAll2 (int value) {
+    while (remove(value));
+}
+
+void output() {
+    Node* tmp = head;
+    while (tmp != nullptr) {
+        std::cout << tmp->data << " ";
+        tmp = tmp->next;
+    }
+    std::cout << std::endl;
+}
+
+void outputReverse() {
+    Node* tmp = tail;
+    while (tmp != nullptr) {
+        std::cout << tmp->data << " ";
+        tmp = tmp->prev;
+    }
+}
 
 int main() {
 
     add(8);
     add(2);
+    //add(4);
     add(3);
     add(4);
     add(10);
     add(1);
+    //add(4);
     bubleSort();
+    //removeAll2(4);
 
     /*addSorted(6);
     addSorted(3);
     addSorted(4);
+    addSorted(8);
     addSorted(5);
     addSorted(10);
-    addSorted(8);
     addSorted(7);
-    addSorted(2);*/
+    addSorted(2);
+    remove(4);*/
 
 
 
     output();
+    outputReverse();
+
     return 0;
 }
